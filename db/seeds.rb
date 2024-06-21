4e624836eedb87dffd645c1e9873c1a889509e24
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-06-21 09:00:58 -0300

    feat: added todas aulas de 06 até 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.create!({
+  name: 'Sample Property',
+  description: 'asf34daf',
+  headline: 'asfd345af',
+  address_1: 'asfd345af',
+  address_2: 'asf435daf',
+  city: 'asfd345af',
+  state: 'asfd345af',
+  country: 'asf345daf',
+})
+
+Property.create!({
+  name: 'Sample Property 2',
+  description: 'wr',
+  headline: 'asd',
+  address_1: 'asfd345wraf',
+  address_2: 'asd',
+  city: 'ewr',
+  state: 'er',
+  country: 'er',
+})
+
+Property.create!({
+  name: 'wr',
+  headline: 'asd',
+  address_1: 'asfd345wraf',
+  address_2: 'asd',
+  city: 'ewr',
+  state: 'er',
+  country: 'er',
+})
