71657372a17e3f4d7a8eac0f5c8e93698c8a5ed7
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-06-29 13:53:21 -0300

    feat: added items do reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_26_180253) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_29_150947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,7 +82,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_26_180253) do
     t.datetime "updated_at", null: false
     t.index ["property_id"], name: "index_reservations_on_property_id"
     t.index ["user_id", "property_id", "checkin_date", "checkout_date"], name: "add_index_to_reservations", unique: true
-    t.index ["user_id", "property_id", "checkin_date", "checkout_date"], name: "idx_on_user_id_property_id_checkin_date_checkout_da_7282e2dfd7", unique: true
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
@@ -111,6 +110,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_26_180253) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
