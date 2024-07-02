b0a78e95cc667436b4f0f1c0836f9d0ae66f0521
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-02 16:15:14 -0300

    data: added modal do reviews e amenities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_06_30_154048) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_02_153316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_06_30_154048) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
     t.index ["name"], name: "index_amenities_on_name", unique: true
   end
 
