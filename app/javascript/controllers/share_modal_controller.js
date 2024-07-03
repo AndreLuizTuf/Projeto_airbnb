5de48ff8105002b071d3edc8f130f81955b8e3c3
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-03 16:07:26 -0300

    link: atualizaçãolink

--- app/javascript/controllers/share_modal_controller.js
+++ app/javascript/controllers/share_modal_controller.js
@@ -16,25 +16,31 @@ export default class extends Controller {
 
   twitterShare() {
     // TODO: Implement on your own.
+    window.open(`https://twitter.com/intent/tweet?url=${this.element.dataset.shareUrl}`);
   }
 
   facebookShare() {
     // TODO: Implement on your own.
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${this.element.dataset.shareUrl}`);
   }
 
   emailShare() {
     // TODO: Implement on your own.
+    window.open(`mailto:?subject=Check out this amazing property: ${this.element.dataset.shareUrl}`);
   }
 
   embedLink() {
     // TODO: Implement on your own.
+    window.open(`https://www.embedgoogle.com/embed?url=${this.element.dataset.shareUrl}`);
   }
 
   messengerShare() {
     // TODO: Implement on your own.
+    window.open(`https://www.facebook.com/dialog/send?app_id=YOUR_APP_ID&link=${this.element.dataset.shareUrl}`);
   }
 
   messagesShare() {
     // TODO: Implement on your own.
+    window.open(`https://www.facebook.com/dialog/send?app_id=YOUR_APP_ID&link=${this.element.dataset.shareUrl}&message=Check out this amazing property: ${this.element.dataset.shareUrl}`);
   }
 }
