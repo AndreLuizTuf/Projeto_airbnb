a8a214aa4cdcfb6679de921298b7001a9d2b52a0
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-05 11:07:41 -0300

    feat: added booking confirmation page setup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :wishlists, only: [:create, :destroy] 
   end
 
-  resources :properties, only: [:show]
+  resources :properties, only: [:show] do 
+    resources :bookings, only: [:new]
+  end
 end
