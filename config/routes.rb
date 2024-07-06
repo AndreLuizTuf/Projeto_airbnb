5178ef27b1d039efe779fde57938f8050b386b3c
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-06 18:45:48 -0300

    fetch: added page template to profile

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   get "booking_payments/success", to: "booking_payments#success"
 
   resources :wishlists, only: [:index]
+
+  resources :profiles, only: [:show]
 end
