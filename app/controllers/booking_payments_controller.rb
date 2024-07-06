1c36d448734ac24fa00283565756347e526b762c
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-06 18:29:25 -0300

    fetch: Payments to sripe finish

--- app/controllers/booking_payments_controller.rb
+++ app/controllers/booking_payments_controller.rb
@@ -1,16 +1,22 @@
 class BookingPaymentsController < ApplicationController
   def create
-    @property = Property.find(booking_payment_params[:property_id])
+    @property = Property.find(booking_payments_params[:property_id])
     
     stripe_price = Stripe::Price.create({
       currency: 'usd',
-      uni_amount: Money.from_amount(BigDecimal(booking_payment_params[:total_amount])).cents,
+      unit_amount: Money.from_amount(BigDecimal(booking_payments_params[:total_amount])).cents,
       product_data: {
         name: @property.name
       },
     })
 
-    success_url = url_for(controller: 'booking_payments', action: 'success', only_path: false)
+    success_url = url_for(
+      controller: 'booking_payments', 
+      action: 'success', 
+      only_path: false, 
+      booking_params: booking_payments_params.except(:stripeToken)
+    )
+
     stripe_session = Stripe::Checkout::Session.create({
       success_url: success_url,
       line_items: [
@@ -26,18 +32,32 @@ class BookingPaymentsController < ApplicationController
   end
   
   def success
-    # Add reservation
-    # Add payment details offline
-    raise
+    booking_params = params[:booking_params]
+
+    reservation = Reservation.create!(
+      user_id: current_user.id,
+      property_id: booking_params[:property_id],
+      checkin_date: booking_params[:checkin_date],
+      checkout_date: booking_params[:checkout_date]
+    )
+
+    payment = Payment.create!(
+      reservation_id: reservation.id,
+      base_fare: Money.from_amount(BigDecimal(booking_params[:base_fare])),
+      service_fee: Money.from_amount(BigDecimal(booking_params[:service_fee])),
+      total_amount: Money.from_amount(BigDecimal(booking_params[:total_amount])),
+    )
+
+    # TODO: redirect to all bookings page
+    redirect_to root_path
   end
 
   private
 
-  def booking_payment_params
-    params.permitt(
+  def booking_payments_params
+    params.permit(
       :stripeToken,
       :property_id,
-      :user_id,
       :checkin_date,
       :checkout_date,
       :base_fare,
