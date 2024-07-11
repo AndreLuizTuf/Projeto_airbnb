e8db8475acd5602358a627cfddd0e0c3ffb71dff
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-11 15:15:10 -0300

    feat: profile auth fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
   def show
+    before_action :authenticate_user!
+  end
+
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :wishlists, only: [:index]
 
-  resources :profiles, only: [:show]
+  resources :profiles, only: [:index]
 
-  resources :reservations, only: [:show]
+  resources :reservations, only: [:index]
 end
