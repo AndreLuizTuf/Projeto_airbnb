17f27d48ea33e60125e303406c4978ba17ede5cf
AndreLuizTuf <andre.luiz.a.f@hotmail.com>
2024-07-12 11:53:37 -0300

    optimisation for reviews count

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -1,28 +1,28 @@
-import { Controller } from 'hotwired/stimulus';
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  
+  setRating(e) {
+    const rating = e.currentTarget.dataset.rating;
+    const ratingCategory = e.currentTarget.dataset.category;
+    const rootElement = e.currentTarget.parentNode
+    const ratingInput = rootElement.querySelector(`input[data-rating-target="${ratingCategory}"]`); 
 
-    setRating(e) {
-        const rating = e.currentTarget.dataset.rating;
-        const ratingCategory = e.currentTarget.dataset.category;
-        const rootElement = e.currentTarget.parentNode
-        const ratingInput = rootElement.querySelector(`input[data-rating-target="${ratingCategory}"]`);
+    const stars = rootElement.querySelectorAll(`[data-rating-target="star"]`); 
 
-        const stars = rootElement.querySelectorAll(`[data-rating-target="stars"]`);
+    stars.forEach(star => {
+      if(star.dataset.rating <= rating){
+        star.classList.remove('fill-gray-300')
+        star.classList.add('fill-amber-300')
+      }
+      else{
+        star.classList.remove('fill-amber-300')
+        star.classList.add('fill-gray-300')
+      }
+    });
 
-        stars.forEach(star => {
-            if (star.dataset.rating <= rating) {
-                star.classList.remove('fill-gray-300');
-                star.classList.add('fill-gray-300');
-            }
-            else {
-                star.classList.remove('fill-gray-300');
-                star.classList.add('fill-gray-500');
-            }
-        })
-
-        if (ratingInput) {
-            ratingInput.value = rating;
-        }
+    if (ratingInput) {
+      ratingInput.value = rating;
     }
+  }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_07_11_142937) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_12_125945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,8 +100,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_07_11_142937) do
     t.datetime "updated_at", null: false
     t.integer "price_cents"
     t.string "price_currency"
-    t.integer "reviews_count"
-    t.decimal "average_final_rating"
+    t.integer "reviews_count", default: 0
+    t.decimal "average_final_rating", default: "0.0"
     t.integer "guest_count", default: 0
     t.integer "bedroom_count", default: 0
     t.integer "bed_count", default: 0
